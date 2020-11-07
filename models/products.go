482afb9e8672d9ddfb993ac0d07827e82a6894a2
lpmatos <lpmatos@stefanini.com>
2020-11-07 14:40:03 -0300

    feat: product model and db

---
+++ models/products.go
@@ -0,0 +1,35 @@
+package models
+
+// Product is a struct representation to a product in E-Commerce.
+type Product struct {
+	ID     int
+	Name   string
+	Desct  string
+	Price  float64
+	Amount int
+}
+
+// GetAllProducts function
+func GetAllProducts() {
+
+}
+
+// CreateNewProduct function
+func CreateNewProduct() {
+
+}
+
+// DelProduct function
+func DelProduct() {
+
+}
+
+// EditProduct function
+func EditProduct() {
+
+}
+
+// UpdateProduct function
+func UpdateProduct() {
+
+}
